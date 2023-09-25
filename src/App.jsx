c8a54608c736a87b44554d11bd6bb6ffabc56149
Ana Virginia <ana.vlc.ara@gmail.com>
2023-09-25 19:49:03 -0300

    Continuação das alterações referente ao clima: Temperatura: max e min, descição do tempo, latitude e longitude, humidade e velocidade do vento.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import axios from "axios";
 function App() {
     const [search, setSearch] = useState("");
     const [temp, setTemp] = useState(0);
+    const [tempMin, setTempMin] = useState(0);
+    const [tempMax, setTempMax] = useState(0);
     const [weatherDescription, setWeatherDescription] = useState("");
     const [long, setLong] = useState(0);
     const [lat, setLat] = useState(0);
+    const [humidity, setHumidity] = useState(0);
+    const [windSpeed, setWindSpeed] = useState(0);
 
     const searchTyped = () => {
         if (search.trim() === "") {
@@ -20,14 +24,22 @@ function App() {
             .get(`${import.meta.env.VITE_BASE_URL}weather?q=${search}&units=metric&appid=${import.meta.env.VITE_API_KEY}`)
             .then((res) => {
                 const temp = res.data.main.temp;
+                const tempMin = res.data.main.temp_min;
+                const tempMax = res.data.main.temp_max;
                 const weatherDescription = res.data.weather[0].description;
                 const longitude = res.data.coord.lon;
                 const latitude = res.data.coord.lat;
+                const humidity = res.data.main.humidity;
+                const windSpeed = res.data.wind.speed;
                 
                 setTemp(temp)
+                setTempMin(tempMin)
+                setTempMax(tempMax)
                 setWeatherDescription(weatherDescription);
                 setLong(longitude)
                 setLat(latitude)
+                setHumidity(humidity)
+                setWindSpeed(windSpeed)
             })
             .catch((err) => {
                 console.error(err);
@@ -47,9 +59,13 @@ function App() {
                 />
                 <button onClick={searchTyped}>Buscar</button>
                 <p>Temperatura: {temp}</p>
-                <p>Descrição do Tempo: {weatherDescription}</p>
+                <p>Temperatura mínima: {tempMin}</p>
+                <p>Temperatura máxima: {tempMax}</p>
+                <p>Descrição do tempo: {weatherDescription}</p>
                 <p>Longitude: {long}</p>
                 <p>Latitude: {lat}</p>
+                <p>Humidade: {humidity}</p>
+                <p>Velocidade do vento: {windSpeed}</p>
             </main>
         </div>
     );
